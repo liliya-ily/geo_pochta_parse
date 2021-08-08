a36d01c84f26a6886521c03746cba77237d2da6e
Liliya ILY <lylyshaunique@gmail.com>
2021-08-08 23:15:53 +0300

    working parsing

--- start.py
+++ start.py
@@ -1,76 +1,111 @@
+import time
 from selenium.webdriver import Chrome
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
-chrome_options = Options()
-driver = webdriver.Chrome('D:\Dev\selenium\chromedriver.exe')
-import time
-import logging
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+# Настройка работы selenium
+chrome_options = Options()
+driver = webdriver.Chrome('geo_pochta_parse\chromedriver.exe')
+driver.set_window_size(1920, 1080)
 
+# Вход на сайт и обход начального обучения 
 url = "http://geo.pochta.ru/"
 driver.get(url)
 driver.get(url)
-time.sleep(10)
-tutorial_xpath = driver.find_element_by_xpath('//*[@id="app"]/div[2]/div[1]/div[1]/div/div/div[3]/div/div/div/div[1]/i')
+
+tutorial_xpath = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div[2]/div[1]/div[1]/div/div/div[3]/div/div/div/div[1]/i')))
 tutorial_xpath.click()
 time.sleep(5)
-one_xpath = driver.find_element_by_xpath('//*[@id="app"]/div[2]/div[1]/div[2]/div/div/div[2]/div/ul/li[1]')
-one_xpath.click()
 
-# actions = ActionChains(driver)
-# actions.move_by_offset(300, 150).perform()
-# time.sleep(5)
-# actions.click().perform()
+# Отдаление на карте
+smaller__xpath = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH,  '//*[@id="map-osm"]/div[2]/div[2]/div/a[2]')))
+smaller__xpath.click()
+time.sleep(5)
+smaller__xpath.click()
+time.sleep(5)
 
-# actions.move_by_offset(220, 110).perform()
-# time.sleep(5)
-# actions.click().perform()
-ActionChains(driver).move_by_offset(700, 200).click().perform()
-for i in range(10, 90, 10):
-    ActionChains(driver).move_by_offset(10, i).click().perform()
-    # ActionChains(driver).reset_actions()
-    time.sleep(5)
-time.sleep(10)
-take_hidden_window = driver.find_element_by_xpath('//*[@id="app"]/div[2]/div[1]/div[3]/div/div/div[5]/span[2]/div/span')
-take_hidden_window.click()
+# Переключение получение данных с одной точки на контур
+circuit = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH,  '//*[@id="app"]/div[2]/div[1]/div[2]/div/div/div[2]/div/ul/li[3]')))
+circuit.click()
 
-total = []
+# Границы проведения контуров точек
+left_up = [524, 110]
+right_up = [1374, 110]
+left_down = [524, 910]
+right_down = [1374, 910]
 
-quote_text = driver.find_elements_by_class_name('element-addresses')
-print('тут работает element')
-print(quote_text)
-quote_text = driver.find_elements_by_class_name('unselectable')
-print('тут работает unselectable')
-print(quote_text)
+# Заполнение выбранной области контурами
+while left_up[1] < left_down[1]:
 
-for quote in quote_text:
-        # quote_text = quote.find_element_by_class_name('column-1 column-row-header ng-binding')
-        print(quote)
-        # total.append(quote_text)
+    # левая верхняя точка
+    ActionChains(driver).move_by_offset(*left_up).click().perform()
 
+    # правая верхняя точка
+    time.sleep(1)
+    ActionChains(driver).reset_actions()
+    ActionChains(driver).move_by_offset(*right_up).click().perform()
+    right_up[1] += 20
+
+    # правая нижняя точка
+    time.sleep(1)
+    ActionChains(driver).reset_actions()
+    ActionChains(driver).move_by_offset(*right_up).click().perform()
+
+    # левая нижняя точка
+    time.sleep(1)
+    ActionChains(driver).reset_actions()
+    ActionChains(driver).move_by_offset(
+        left_up[0], right_up[1]).click().perform()
+
+    # левая верхняя точка
+    time.sleep(1)
+    ActionChains(driver).reset_actions()
+    ActionChains(driver).move_by_offset(*left_up).click().perform()
+
+    left_up[1] += 20
+    ActionChains(driver).reset_actions()
+    time.sleep(2)
+
+# Появление скрытого окна с адресами и ящиками
+hidden_address_window = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH,  '//*[@id="app"]/div[2]/div[1]/div[3]/div/div/div[5]/span[2]/div/span')))
+hidden_address_window.click()
+
+# Получение и запись данных из скрытого окна 
+table_xpath = '//*[@id="app"]/div[2]/div[2]/div[5]/div/div[2]/div[2]/div/table/tbody/'
+time.sleep(15)
+columns = 3
+rows = len(driver.find_elements_by_xpath(table_xpath + 'tr'))
+# Необязательный вывод для проверки, что данные найдены
+print("rows - ", rows)   
+
+df = pd.DataFrame(columns=['street', 'house', 'amount_mailboxes'])
+
+# Необязательная переменная для отслеживания работоспособности набора данных
+i = 0
+
+for row in range(2, rows+1):
+    street = driver.find_element_by_xpath(
+        table_xpath + "tr["+str(row)+"]/td[1]").text
+    house = driver.find_element_by_xpath(
+        table_xpath + "tr["+str(row)+"]/td[2]").text
+    amount_mailboxes = driver.find_element_by_xpath(
+        table_xpath + "tr["+str(row)+"]/td[3]").text
+    df.loc[i] = street, house, amount_mailboxes
+    i += 1
+    if (i % 1000) == 0:
+        print(i)
+
+df.to_csv('test.csv', index=False)
+
+# Закрыть окно браузера
 # driver.close()
-df = pd.DataFrame(total,columns=['quote'])
-df.to_csv('quoted.csv')
-# pages = 11
-
-
-# for page in range(1,pages):
-    
-#     url = "http://quotes.toscrape.com/js/page/" + str(page) + "/"
-
-#     driver.get(url)
-    
-#     quotes = driver.find_elements_by_class_name("quote")
-#     for quote in quotes:
-#         quote_text = quote.find_element_by_class_name('text').text[1:-2]
-#         author = quote.find_element_by_class_name('author').text
-#         new = ((quote_text,author))
-#         total.append(new)
-
-# driver.close()
-# df = pd.DataFrame(total,columns=['quote','author'])
-# df.to_csv('quoted.csv')
